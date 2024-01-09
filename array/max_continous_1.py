e18de4c5ee4f6286b28863d0b580a2786ed74725
zzt <519174247@qq.com>
2024-01-09 18:13:27 +0800

    完成485、283，然后27还没完成，240109

--- array/max_continous_1.py
+++ array/max_continous_1.py
@@ -1,1 +1,39 @@
 # 对应leetcode 485
+'''
+    给定一个二进制数组，即元素只有0和1，计算其中最大的连续1的个数
+    例子：input = [1,1,0,1,1,1] output = 3
+'''
+# 单次测试
+# input = [1,1,0,1,1,1,0,1,1,1,1]
+# count = 0
+# result = 0
+# for i in input:
+#     if i == 1:
+#         count += 1
+#     else:
+#         result = count
+#         count = 0
+# output = max(result,count)
+# print(output)
+
+# 写成类的形式，封装
+from typing import List # 若不引入该类型，无法直接使用
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        if nums is None or len(nums) == 0:
+            return 0
+        count = 0 
+        result = 0
+        for i in nums:
+            if i == 1:
+                count += 1
+            else:
+                result = count
+                count = 0
+        return max(result,count)
+
+if __name__ == '__main__':
+    input = [1,1,0,1,1,1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1]
+    solution = Solution()
+    output = solution.findMaxConsecutiveOnes(input)
+    print(output)
--- array/move_zero.py
+++ array/move_zero.py
@@ -1,1 +1,42 @@
 # 对应leetcode 283
+'''
+    给定一个数组，需要将数组中所有的0移动到数组的末尾，同时保持非零元素的相对顺序不变
+    必须要在原数组上操作，不能拷贝额外数组，尽量减少操作次数
+'''
+
+# 单次测试
+# input = [0,1,0,3,12,0,0,6,7,8]
+# not_zero_index = 0
+# count_zero = 0
+# for i in range(0,len(input)):
+#     if input[i] != 0:
+#         input[not_zero_index] =input[i]
+#         not_zero_index += 1
+#     else:
+#         count_zero += 1
+# input[(len(input)-count_zero):] = [0] * count_zero
+# print(input)
+
+# 写成类的形式
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> List[int]:
+        not_zero_index = 0
+        for num in nums:
+            if num != 0:
+                nums[not_zero_index] = num
+                not_zero_index += 1
+        for i in range(not_zero_index,len(nums)):
+            nums[i] = 0
+        return nums
+        # 或者如下写法也可以
+        # nums[not_zero_index:] = [0] * (len(nums)-not_zero_index-1)
+        # return nums
+
+if __name__ == '__main__':
+    solution = Solution()
+    input = [0,1,0,3,12,0,0,6,7,8]
+    output = solution.moveZeroes(input)
+    print(output)
+    
+# 小结，单次测试中引入了两个计数器，不如答案中只有一个计数器方便
