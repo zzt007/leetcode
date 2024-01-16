68c3b402c3dc3008b78f0f4d3acb61c06f7561c3
zzt <519174247@qq.com>
2024-01-16 10:41:50 +0800

    更新heap部分692解法

--- heap/find_kFrequence_word.py
+++ heap/find_kFrequence_word.py
@@ -22,20 +22,34 @@ class Solution:
         # print(self.hashtable)
         for word in self.hashtable:
             heapq.heappush(self.minheap,(self.hashtable[word],word))
-            if len(self.minheap) > k:
-                heapq.heappop(self.minheap)
+        print(self.minheap)
         
         res = []
-        while len(self.minheap) > 0:
-            temp = heapq.heappop(self.minheap)
-            print(temp) # (1,'coding')
-            res.append(temp[1])
-            
-        res.reverse()
+        self.minheap = sorted(self.minheap,key= lambda x:(-x[0],x[1]))
+        print(self.minheap)
+        for i in range(k):
+            res.append(self.minheap[i][1])
+        # print(res)
         return res
     
 if __name__ == "__main__":
     test = Solution()
-    words = ["i","love","leetcode","i","love","coding"]
-    k = 2
+    words =["the","day","is","sunny","the","the","the","sunny","is","is"]
+    k = 4
     print(test.findKFrequenceWord(words,k))
+
+
+# 关于lambda表达式和sort与lambda合用的，可以参考如下两篇
+# https://blog.csdn.net/qq_41500249/article/details/106244810
+# https://blog.csdn.net/PY0312/article/details/88956795
+    
+
+# 优秀解法，不需要hashtable进行元素统计，直接利用collections模块自带的counter
+# 用法参考：https://blog.csdn.net/chl183/article/details/106956807
+from collections import Counter # 可以快速进行元素统计，返回一个字典，键为元素，值为个数
+class Solution(object):
+    def topKFrequent(self, words, k):
+        count = Counter(words)
+        heap = [(-freq, word) for word, freq in count.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in range(k)]
